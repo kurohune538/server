56bc4fb7048760a3fd81032e61147e898b562382
Shinnosuke Komiya <kurohune538@gmail.com>
2015-09-30 04:15:23 +0900

    add post function

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
+var pg = require('pg');
+var connectionString = require(path.join(__dirname, 'config.json')).database_url;
+var username = require(path.join(__dirname, 'config.json')).db_user_name;
+var password = require(path.join(__dirname, 'config.json')).db_pass;
+var host = require(path.join(__dirname, 'config.json')).host;
+var database_name = require(path.join(__dirname, 'config.json')).database_name;
+var client = new pg.Client({
+  user: username,
+  password: password,
+  host: host,
+  port: 5432,
+  database: database_name,
+  ssl: true
+});
+
 app.set("view engine", "jade");
 app.set("views", path.join(__dirname, "views"));
 // app.use(favicon(path.join(__dirname, "static", "favicon.ico")));
@@ -21,6 +36,29 @@ app.use(cookieParser());
 
 app.get("/yokome_admin", (req, res) => {
   res.render("post");
+});
+app.post("/yokome_admin/add", (req, res) => {
+  client.connect((err) => {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('SELECT id FROM users WHERE pass=$1',[req.body.password], (err, result) => {
+      if(err) {
+        return console.error('error running query', err);
+      }
+      // if(result.rows.length > 1){
+        client.query('INSERT INTO posts(title, description, like_count) VALUES($1, $2, $3)',[req.body.title, req.body.comment, 0], (err, result) => {
+          console.log(result);
+          client.end();
+          res.send("OK");
+        });
+      // } else {
+      //   return console.error('パスワードが違います', err);
+      // }
+      console.log(result);
+
+    });
+  });
 });
 
 app.use(st({
